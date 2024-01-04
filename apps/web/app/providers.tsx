348d62731a89b3adda1b5abc234d225843e8d9f4
Daun <iam@daun.io>
2024-01-04 16:37:42 +0900

    add inapp browser bypass

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { ThemeProvider } from "next-themes";
 import { AuthenticationProvider } from "./authentication-provider";
+import { useInAppBrowserBypassing } from "./lib/hooks/use-in-app-browser-bypass";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useInAppBrowserBypassing();
   return (
     <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
       <AuthenticationProvider>{children}</AuthenticationProvider>
