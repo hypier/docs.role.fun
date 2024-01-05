98be98c61c2f6086c96b8b5def53bf04893ba259
Daun <iam@daun.io>
2024-01-05 13:53:14 +0900

    reduce description line clamp

--- apps/web/components/cards/character-card.tsx
+++ apps/web/components/cards/character-card.tsx
@@ -98,7 +98,7 @@ const CharacterCard = (props: {
             <CardDescription
               className={`${
                 props.cardImageUrl ? "text-white" : "text-foreground"
-              } z-[3] line-clamp-3 select-none text-xs duration-200 hover:line-clamp-none group-hover:opacity-80`}
+              } z-[3] line-clamp-2 select-none text-xs duration-200 hover:line-clamp-none group-hover:opacity-80`}
             >
               {props.description}
             </CardDescription>
