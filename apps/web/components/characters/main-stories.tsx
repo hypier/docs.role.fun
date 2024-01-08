57a710de6dbcf54879070b5de7d725004b870927
Daun <iam@daun.io>
2024-01-08 14:46:56 +0900

    fix layout

--- apps/web/components/characters/main-stories.tsx
+++ apps/web/components/characters/main-stories.tsx
@@ -28,7 +28,7 @@ export const MainStories = () => {
   }, [inView, loadMore]);
   return (
     <section className="flex flex-col gap-4 lg:gap-8">
-      <div className="self-center px-4 font-medium lg:px-0">Stories</div>
+      <div className="px-4 font-medium lg:px-0">Stories</div>
       <div className="border-y bg-background p-2 py-12 !text-xs lg:rounded-lg lg:border lg:shadow-lg">
         <Carousel
           opts={{
