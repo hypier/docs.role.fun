37772cb18e5fa9b3701440d0bde30bae1de1988c
Daun <iam@daun.io>
2024-01-11 17:54:21 +0900

    add metadata

--- apps/web/app/models/page.tsx
+++ apps/web/app/models/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { constructMetadata } from "../lib/utils";
 import Models from "./models";
+
+export const metadata = constructMetadata({
+  title: "Models - Discover Best AI Models",
+  description: "Explore and find the best models to enhance your AI companion.",
+});
 
 export default function Page(): JSX.Element {
   return (
