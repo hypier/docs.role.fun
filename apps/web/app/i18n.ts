547e4c232ed46dca1aa814d67522fe04a7eaa2cf
Daun <iam@daun.io>
2024-01-17 13:23:15 +0900

    translate with backend

--- apps/web/app/i18n.ts
+++ apps/web/app/i18n.ts
@@ -1,102 +1,37 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import translationKo from "../public/locales/ko.json";
-import translationJa from "../public/locales/ja.json";
-import translationZh from "../public/locales/zh.json";
-import translationAr from "../public/locales/ar.json";
-import translationCs from "../public/locales/cs.json";
-import translationDa from "../public/locales/da.json";
-import translationDe from "../public/locales/de.json";
-import translationEl from "../public/locales/el.json";
-import translationEs from "../public/locales/es.json";
-import translationFi from "../public/locales/fi.json";
-import translationFr from "../public/locales/fr.json";
-import translationIt from "../public/locales/it.json";
-import translationNl from "../public/locales/nl.json";
-import translationNo from "../public/locales/no.json";
-import translationPl from "../public/locales/pl.json";
-import translationPt from "../public/locales/pt.json";
-import translationRo from "../public/locales/ro.json";
-import translationRu from "../public/locales/ru.json";
-import translationSv from "../public/locales/sv.json";
-import translationUk from "../public/locales/uk.json";
+import { initReactI18next } from "react-i18next";
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
+  .use(Backend) // use http backend to load translation files
+  .use(LanguageDetector) // detect user language
+  .use(initReactI18next) // pass the i18n instance to react-i18next.
   .init({
-    resources: {
-      en: {
-        translation: {},
-      },
-      ko: {
-        translation: translationKo,
-      },
-      ja: {
-        translation: translationJa,
-      },
-      zh: {
-        translation: translationZh,
-      },
-      ar: {
-        translation: translationAr,
-      },
-      cs: {
-        translation: translationCs,
-      },
-      da: {
-        translation: translationDa,
-      },
-      de: {
-        translation: translationDe,
-      },
-      el: {
-        translation: translationEl,
-      },
-      es: {
-        translation: translationEs,
-      },
-      fi: {
-        translation: translationFi,
-      },
-      fr: {
-        translation: translationFr,
-      },
-      it: {
-        translation: translationIt,
-      },
-      nl: {
-        translation: translationNl,
-      },
-      no: {
-        translation: translationNo,
-      },
-      pl: {
-        translation: translationPl,
-      },
-      pt: {
-        translation: translationPt,
-      },
-      ro: {
-        translation: translationRo,
-      },
-      ru: {
-        translation: translationRu,
-      },
-      sv: {
-        translation: translationSv,
-      },
-      uk: {
-        translation: translationUk,
-      },
+    backend: {
+      // path where resources get loaded from, or a function
+      // returning a path:
+      // function(lngs, namespaces) { return customPath; }
+      // the returned path will interpolate lng, ns if provided like giving a static path
+      loadPath: "/locales/{{lng}}.json",
+
+      // your backend server supports multiloading
+      // /locales/resources.json?lng=de+en&ns=translation
+      // Adapter is needed to enable MultiLoading https://github.com/i18next/i18next-multiload-backend-adapter
+      // Returned JSON structure in this case is
+      // {
+      //  lang : {
+      //   namespaceA: {},
+      //   namespaceB: {},
+      //   ...etc
+      //  }
+      // }
+      allowMultiLoading: false, // set loadPath: '/locales/resources.json?lng={{lng}}&ns={{ns}}' to adapt to multiLoading
     },
     lng: "en",
     fallbackLng: "en",
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
     },
   });
 
