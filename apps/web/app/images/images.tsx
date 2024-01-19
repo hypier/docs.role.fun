5f638f3d1c5ce09d6c44072d74c3f7f65d3e2c88
Daun <iam@daun.io>
2024-01-19 11:40:11 +0900

    fix

--- apps/web/app/images/images.tsx
+++ apps/web/app/images/images.tsx
@@ -114,7 +114,7 @@ const Images = () => {
         </Button>
       ) : (
         <Link href="/sign-in" className="w-full">
-          <Button className="h-7 gap-1 text-xs">{t("Generate")}</Button>
+          <Button className="h-7 w-full gap-1 text-xs">{t("Generate")}</Button>
         </Link>
       )}
       <FormMessage />
