a6f1dfe80977b13c2bd81e56a12e869f66e837de
Daun <iam@daun.io>
2024-01-22 11:44:14 +0900

    fix

--- apps/web/components/characters/character-form.tsx
+++ apps/web/components/characters/character-form.tsx
@@ -489,7 +489,7 @@ export default function CharacterForm() {
                         ) : (
                           <>
                             {t("Generate")}
-                            <Crystal className="h-4 w-4" /> x 1
+                            <Crystal className="h-4 w-4" /> x 9
                           </>
                         )}
                       </Button>
