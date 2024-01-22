a17856d7c21895c225421a36de56773d6d9050c5
Daun <iam@daun.io>
2024-01-22 15:13:14 +0900

    score characters hourly

--- apps/web/convex/crons.ts
+++ apps/web/convex/crons.ts
@@ -2,9 +2,9 @@ import { cronJobs } from "convex/server";
 import { internal } from "./_generated/api";
 
 const crons = cronJobs();
-crons.daily(
+crons.hourly(
   "score characters",
-  { hourUTC: 0, minuteUTC: 0 },
+  { minuteUTC: 0 },
   internal.characters.scoreAll,
 );
 export default crons;
