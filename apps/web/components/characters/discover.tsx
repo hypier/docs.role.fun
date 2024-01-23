50b5e118323bfe55d3b8ef9e9d248eeec870ce62
Daun <iam@daun.io>
2024-01-23 09:33:24 +0900

    fix

--- apps/web/components/characters/discover.tsx
+++ apps/web/components/characters/discover.tsx
@@ -69,7 +69,7 @@ const Discover = () => {
     setCount(_api.scrollSnapList().length);
     setCurrent(_api.selectedScrollSnap() + 1);
     if (_api.selectedScrollSnap() + 1 >= _api.scrollSnapList().length - 10) {
-      if (!me?.name && count > 21) {
+      if (!me?.name && count > 31) {
         setIsSignInModalOpen(true);
       } else {
         loadMore(10);
