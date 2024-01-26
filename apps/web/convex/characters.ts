88fe2d6fff60ad32697ae9852c2ebfa817ab0d97
Daun <iam@daun.io>
2024-01-27 08:24:58 +0900

    improve

--- apps/web/convex/characters.ts
+++ apps/web/convex/characters.ts
@@ -515,7 +515,7 @@ export const scoreAll = internalMutation({
       const numChats = character?.numChats || 1;
       await ctx.db.patch(character._id, {
         score:
-          numChats /
+          (numChats * 2) /
           Math.pow(
             (new Date().getTime() - createdAt + 2 * 60 * 60 * 1000) /
               (7 * 24 * 60 * 60 * 1000),
