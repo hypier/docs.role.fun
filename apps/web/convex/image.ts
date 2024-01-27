907bef1ded28d58f6962adbf666a5b5ab071b1aa
Daun <iam@daun.io>
2024-01-27 15:59:43 +0900

    fix

--- apps/web/convex/image.ts
+++ apps/web/convex/image.ts
@@ -334,7 +334,7 @@ export const generateByPrompt = internalAction(
           image: referenceImage ?? undefined,
           disable_safety_checker: true,
           negative_prompt:
-            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name",
+            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, blushed",
           num_inference_steps: 40,
         },
       });
