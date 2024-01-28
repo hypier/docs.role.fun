1f28bea0e6bd8743993c0dbfba35a77c0fb9815d
Daun <iam@daun.io>
2024-01-29 07:40:03 +0900

    simplify mobile logo

--- packages/ui/src/components/text-logo.tsx
+++ packages/ui/src/components/text-logo.tsx
@@ -1,9 +1,12 @@
+import useMediaQuery from "../hooks/use-media-query";
+
 export default function TextLogo({ className }: { className?: string }) {
+  const { isMobile } = useMediaQuery();
   return (
     <span
       className={`flex items-center font-medium ${className} hover:opacity-50`}
     >
-      openroleplay.ai
+      {isMobile ? "ORP" : "openroleplay.ai"}
     </span>
   );
 }
