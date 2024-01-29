1351773509bef116d1db15b5a714bd477860a1ff
Daun <iam@daun.io>
2024-01-29 09:42:19 +0900

    fix

--- apps/web/app/crystals/payment-dialog.tsx
+++ apps/web/app/crystals/payment-dialog.tsx
@@ -14,9 +14,9 @@ import { useSearchParams } from "next/navigation";
 import { toast } from "sonner";
 
 const stripeKey =
-  process.env.NODE_ENV === "production"
-    ? "pk_live_51OJquFDWbs4J5X5c73h8TpqpRHY5OVpGBiWqia7DkkYMUUAf8yZ5upuDhEK2LRXcFe8qCrlzNLPmkJr0AKVtXn7600AVe2lUmZ"
-    : "pk_test_51OJquFDWbs4J5X5ckbKjSEpcjrgOYTsHxRiOq1frxbahPFDtt0perqP7cWLl8FTUIQ0aVP7dMugvauRxpbT54Wjo004FuhU1Ug";
+  process.env.NODE_ENV === "development"
+    ? "pk_test_51OJquFDWbs4J5X5ckbKjSEpcjrgOYTsHxRiOq1frxbahPFDtt0perqP7cWLl8FTUIQ0aVP7dMugvauRxpbT54Wjo004FuhU1Ug"
+    : "pk_live_51OJquFDWbs4J5X5c73h8TpqpRHY5OVpGBiWqia7DkkYMUUAf8yZ5upuDhEK2LRXcFe8qCrlzNLPmkJr0AKVtXn7600AVe2lUmZ";
 
 const stripePromise = loadStripe(stripeKey);
 
