ae6dafd9266261859ebfebfc63ea4a4225a0c580
Daun <iam@daun.io>
2024-01-29 13:24:29 +0900

    fix

--- apps/web/app/dmca/page.tsx
+++ apps/web/app/dmca/page.tsx
@@ -3,7 +3,7 @@ import Content from "./content.mdx";
 
 export default function Page() {
   return (
-    <div className="container prose mx-auto break-words px-4 dark:prose-invert prose-p:leading-relaxed prose-pre:p-0">
+    <div className="container prose mx-auto mb-16 break-words px-4 dark:prose-invert prose-p:leading-relaxed prose-pre:p-0">
       <Content />
     </div>
   );
--- apps/web/app/safety/page.tsx
+++ apps/web/app/safety/page.tsx
@@ -3,7 +3,7 @@ import Content from "./content.mdx";
 
 export default function Page() {
   return (
-    <div className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 container mx-auto break-words px-4">
+    <div className="container prose mx-auto mb-16 break-words px-4 dark:prose-invert prose-p:leading-relaxed prose-pre:p-0">
       <Content />
     </div>
   );
