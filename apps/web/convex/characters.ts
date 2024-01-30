f844e08d24b0777feca7a4b1399cd26bff3381f0
Daun <iam@daun.io>
2024-01-30 14:12:44 +0900

    bump

--- apps/web/convex/characters.ts
+++ apps/web/convex/characters.ts
@@ -182,7 +182,7 @@ export const listWithHides = query({
   handler: async (ctx, args) => {
     let query = ctx.db
       .query("characters")
-      .withIndex("byNumChats")
+      .withIndex("byScore")
       .filter((q) => q.eq(q.field("isDraft"), false))
       .filter((q) => q.eq(q.field("isBlacklisted"), false))
       .filter((q) => q.neq(q.field("isArchived"), true))
