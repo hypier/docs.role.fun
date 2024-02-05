75c470a392078561535f9e8283ebbcd228da5414
Daun <iam@daun.io>
2024-02-05 20:30:43 +0900

    add more example

--- apps/web/convex/llm.ts
+++ apps/web/convex/llm.ts
@@ -431,7 +431,7 @@ export const generateFollowups = internalAction({
         const userRole =
           persona && "name" in persona ? persona?.name : username;
         const userPrefix = `${userRole}: `;
-        for (let i = 1; i <= (user?.subscriptionTier === "plus" ? 2 : 1); i++) {
+        for (let i = 1; i <= (user?.subscriptionTier === "plus" ? 3 : 1); i++) {
           const instruction = getInstruction(
             character,
             persona,
@@ -444,7 +444,7 @@ export const generateFollowups = internalAction({
                 ? model
                 : i === 2
                   ? "recursal/eagle-7b"
-                  : "nousresearch/nous-capybara-7b:free",
+                  : "huggingfaceh4/zephyr-7b-beta:free",
             stream: false,
             messages: [
               {
