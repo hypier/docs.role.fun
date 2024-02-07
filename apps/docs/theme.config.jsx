1185d4e0234be3c3385043cea7f342a3bc1fe7c5
Daun <iam@daun.io>
2024-02-07 11:37:59 +0900

    improve theme

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,5 +1,14 @@
 export default {
-  logo: <span>Openroleplay.ai</span>,
+  logo: <span className="font-medium">📙 Openroleplay.ai Docs</span>,
   docsRepositoryBase:
     "https://github.com/Open-Roleplay-AI/openroleplay.ai/tree/main/apps/docs",
+  head: (
+    <>
+      <meta property="og:title" content="Openroleplay.ai Docs" />
+      <meta
+        property="og:description"
+        content="Learn how to create engaging characters at Openroleplay.ai"
+      />
+    </>
+  ),
 };
