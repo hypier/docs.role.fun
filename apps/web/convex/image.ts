64a2cc523b963150742a675329909b107b42051d
Daun <iam@daun.io>
2024-02-13 16:18:38 +0900

    rollback

--- apps/web/convex/image.ts
+++ apps/web/convex/image.ts
@@ -342,7 +342,7 @@ export const generateByPrompt = internalAction(
           image: referenceImage ?? undefined,
           disable_safety_checker: true,
           negative_prompt:
-            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, girl",
+            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name",
           num_inference_steps: 40,
         },
       });
