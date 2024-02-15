a26f8504e2c8332ca443fae24c4cc76f1a851d33
Daun <iam@daun.io>
2024-02-15 15:12:40 +0900

    refactor

--- apps/web/app/dialog.tsx
+++ apps/web/app/dialog.tsx
@@ -71,6 +71,7 @@ import {
   useTranslationStore,
 } from "./lib/hooks/use-machine-translation";
 import usePersona from "./lib/hooks/use-persona";
+import React from "react";
 
 export const FormattedMessage = ({
   message,
@@ -80,46 +81,60 @@ export const FormattedMessage = ({
   username?: string;
 }) => {
   const { t } = useTranslation();
-  const baseText = message?.text?.startsWith("Not enough crystals.")
-    ? `${message?.text} [${t("Crystal Top-up")}](/crystals)`
-    : message?.text;
-  const translationText = message?.translation
-    ? `${message?.translation} *[${message?.text.trim()}]*`
-    : "";
-  const textContent = translationText ? translationText : baseText;
-  const formattedText = textContent
-    ?.replaceAll("{{user}}", username)
-    .replace(/#+$/, "");
+
+  const formatBaseText = (message: any) => {
+    return message?.text?.startsWith("Not enough crystals.")
+      ? `${message?.text} [${t("Crystal Top-up")}](/crystals)`
+      : message?.text;
+  };
+
+  const formatTranslationText = (message: any) => {
+    return message?.translation
+      ? `${message?.translation} *[${message?.text.trim()}]*`
+      : "";
+  };
+
+  const replaceUsername = (text: string, username?: string) => {
+    return text?.replaceAll("{{user}}", username ?? "").replace(/#+$/, "");
+  };
+
+  const splitText = (formattedText: string, characterId?: string) => {
+    return characterId
+      ? formattedText?.includes("") ||
+        formattedText?.includes("1.") ||
+        formattedText?.includes("- ")
+        ? [formattedText]
+        : formattedText?.split(/(?<=[.?!])(?![.?!])|\n/)
+      : [formattedText];
+  };
+
+  const baseText = formatBaseText(message);
+  const translationText = formatTranslationText(message);
+  const textContent = translationText || baseText;
+  const formattedText = replaceUsername(textContent, username);
+
   return (
     <div
       className={`mb-1 flex flex-col gap-1 ${
         message?.characterId ? "items-start" : "items-end"
       }`}
     >
-      {(message?.characterId
-        ? formattedText?.includes("```") ||
-          formattedText?.includes("1.") ||
-          formattedText?.includes("- ")
-          ? [formattedText]
-          : formattedText?.split(/(?<=[.?!])(?![.?!])|\n/)
-        : [formattedText]
-      ).map((sentence: string, index: number) => (
-        <>
-          {sentence?.length > 0 && (
-            <div
-              key={index}
-              className={`w-fit whitespace-pre-wrap rounded-xl ${
-                message?.characterId
-                  ? "bg-gradient-to-b from-background to-muted shadow-lg"
-                  : "dark bg-gradient-to-t from-blue-400 to-blue-500 shadow-lg"
-              } px-3 py-2 md:max-w-[36rem] lg:max-w-[48rem]`}
-            >
-              <MemoizedReactMarkdown
-                className="prose break-words text-foreground dark:prose-invert prose-p:m-0 prose-p:leading-relaxed prose-em:select-text prose-em:pr-0.5 prose-em:text-foreground/50 prose-pre:p-0 prose-ol:my-0 prose-ol:flex prose-ol:flex-col prose-ol:gap-1 prose-ul:my-0 prose-ul:flex prose-ul:flex-col prose-ul:gap-1 prose-li:my-0"
-                remarkPlugins={[remarkGfm]}
-                components={{
-                  a({ children, href, target, rel }) {
-                    return (
+      {splitText(formattedText, message?.characterId).map(
+        (sentence: string, index: number) => (
+          <React.Fragment key={index}>
+            {sentence?.length > 0 && (
+              <div
+                className={`w-fit whitespace-pre-wrap rounded-xl ${
+                  message?.characterId
+                    ? "bg-gradient-to-b from-background to-muted shadow-lg"
+                    : "dark bg-gradient-to-t from-blue-400 to-blue-500 shadow-lg"
+                } px-3 py-2 md:max-w-[36rem] lg:max-w-[48rem]`}
+              >
+                <MemoizedReactMarkdown
+                  className="prose break-words text-foreground dark:prose-invert prose-p:m-0 prose-p:leading-relaxed prose-em:select-text prose-em:pr-0.5 prose-em:text-foreground/50 prose-pre:p-0 prose-ol:my-0 prose-ol:flex prose-ol:flex-col prose-ol:gap-1 prose-ul:my-0 prose-ul:flex prose-ul:flex-col prose-ul:gap-1 prose-li:my-0"
+                  remarkPlugins={[remarkGfm]}
+                  components={{
+                    a: ({ children, href, target, rel }) => (
                       <a
                         href={href}
                         rel={rel}
@@ -128,27 +143,27 @@ export const FormattedMessage = ({
                       >
                         {children}
                       </a>
-                    );
-                  },
-                  code({ node, className, children, ...props }: any) {
-                    const match = /language-(\w+)/.exec(className || "");
-                    return (
-                      <CodeBlock
-                        key={Math.random()}
-                        language={(match && match[1]) || ""}
-                        value={String(children).replace(/\n$/, "")}
-                        {...props}
-                      />
-                    );
-                  },
-                }}
-              >
-                {sentence}
-              </MemoizedReactMarkdown>
-            </div>
-          )}
-        </>
-      ))}
+                    ),
+                    code: ({ node, className, children, ...props }) => {
+                      const match = /language-(\w+)/.exec(className || "");
+                      return (
+                        <CodeBlock
+                          key={Math.random()}
+                          language={(match && match[1]) || ""}
+                          value={String(children).replace(/\n$/, "")}
+                          {...props}
+                        />
+                      );
+                    },
+                  }}
+                >
+                  {sentence}
+                </MemoizedReactMarkdown>
+              </div>
+            )}
+          </React.Fragment>
+        ),
+      )}
     </div>
   );
 };
