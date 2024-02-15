cd5ab018661953c04230bcbd5315621e92bbfb2b
Daun <iam@daun.io>
2024-02-15 21:06:42 +0900

    simplify words

--- apps/web/components/user/preference-dialog.tsx
+++ apps/web/components/user/preference-dialog.tsx
@@ -52,9 +52,7 @@ const PreferenceDialog = () => {
           <div className="flex w-full flex-col items-center justify-center">
             <AlertDialogDescription className="flex flex-col items-center justify-center gap-4">
               <div className="flex text-center text-lg">
-                {t(
-                  "Welcome to Openroleplay.ai, your open-source AI character platform.",
-                )}
+                {t("Welcome to Openroleplay.ai.")}
               </div>
             </AlertDialogDescription>
           </div>
