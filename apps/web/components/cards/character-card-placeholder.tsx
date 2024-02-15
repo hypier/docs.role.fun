4c21b2b454daf180afa5df2fa6488da8319830fa
Daun <iam@daun.io>
2024-02-15 19:14:55 +0900

    fix

--- apps/web/components/cards/character-card-placeholder.tsx
+++ apps/web/components/cards/character-card-placeholder.tsx
@@ -5,10 +5,10 @@ const CharacterCardPlaceholder = (props: {}) => {
   return (
     <AspectRatio
       ratio={1 / 1.75}
-      className="group h-full w-full place-content-center rounded-lg duration-200 hover:-translate-y-1 hover:shadow-lg"
+      className="group h-full w-full place-content-center rounded-xl duration-200 hover:-translate-y-1 hover:shadow-lg"
     >
-      <Card className="flex h-full w-full items-end rounded-lg p-2">
-        <div className="pointer-events-none absolute left-0 top-0 h-full w-full rounded-lg bg-muted object-cover" />
+      <Card className="flex h-full w-full items-end rounded-xl p-2">
+        <div className="pointer-events-none absolute left-0 top-0 h-full w-full rounded-xl bg-muted object-cover" />
       </Card>
     </AspectRatio>
   );
