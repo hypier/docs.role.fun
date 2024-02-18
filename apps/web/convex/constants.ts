f639987a18cdd4a810564a0c4c13dd862de1c1df
Daun <iam@daun.io>
2024-02-18 19:36:43 +0900

    fix price

--- apps/web/convex/constants.ts
+++ apps/web/convex/constants.ts
@@ -179,7 +179,7 @@ export const modelData = [
     value: "undi95/toppy-m-7b",
     description: "Uncensored, Toppy M 7B by undi95",
     isNSFW: true,
-    crystalPrice: 0.2,
+    crystalPrice: 1,
   },
   {
     value: "undi95/toppy-m-7b:free",
