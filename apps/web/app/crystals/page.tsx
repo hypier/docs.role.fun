020ee5b2f758ddb46dde0969030c9d1e4ae95409
Daun <iam@daun.io>
2024-02-21 07:41:35 +0900

    fix

--- apps/web/app/crystals/page.tsx
+++ apps/web/app/crystals/page.tsx
@@ -621,7 +621,7 @@ export default function Page() {
             />
           </h2>
           <p className="flex items-center gap-1 text-center text-sm text-muted-foreground">
-            2 x
+            3 x
             <Crystal className="h-4 w-4" />
           </p>
         </div>
