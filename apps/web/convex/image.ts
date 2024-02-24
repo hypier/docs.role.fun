b889a4413d1517794a98396ab27cdc8b33eb8f6a
Daun <iam@daun.io>
2024-02-24 11:19:09 +0900

    improve prompt

--- apps/web/convex/image.ts
+++ apps/web/convex/image.ts
@@ -355,7 +355,7 @@ export const generateByPrompt = internalAction(
           image: referenceImage ?? undefined,
           disable_safety_checker: true,
           negative_prompt:
-            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name",
+            "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, rating: sensitive",
           num_inference_steps:
             model === "lucataco/sdxl-lightning-4step" ? 4 : 40,
         },
