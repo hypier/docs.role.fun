9122757cd8b3c37d983cc57826c2b7f1d5169bca
Daun <iam@daun.io>
2024-02-25 21:01:37 +0900

    hide character button for certain situation

--- apps/web/app/images/image-card.tsx
+++ apps/web/app/images/image-card.tsx
@@ -79,30 +79,30 @@ const ImageDetail = (props: {
               {props?.prompt}
             </DialogOrDrawerDescription>
             <div className="grid w-full grid-cols-3 justify-between gap-2">
+              <Tooltip content={"Create new image by remixing this image"}>
+                <Link
+                  href={`/images?prompt=${props.prompt}&model=${props.model}`}
+                  className="w-full"
+                >
+                  <Button className="w-full gap-1" variant="outline">
+                    <Repeat className="h-4 w-4" />
+                    {t("Remix")}
+                  </Button>
+                </Link>
+              </Tooltip>
               {!props?.isNSFW && (
-                <Tooltip content={"Create new image by remixing this image"}>
+                <Tooltip content={"Create character using this image"}>
                   <Link
-                    href={`/images?prompt=${props.prompt}&model=${props.model}`}
+                    href={`/my-characters/create?cardImageUrl=${props.imageUrl}&description=${props.prompt}`}
                     className="w-full"
                   >
                     <Button className="w-full gap-1" variant="outline">
-                      <Repeat className="h-4 w-4" />
-                      {t("Remix")}
+                      <Plus className="h-4 w-4" />
+                      {t("Character")}
                     </Button>
                   </Link>
                 </Tooltip>
               )}
-              <Tooltip content={"Create character using this image"}>
-                <Link
-                  href={`/my-characters/create?cardImageUrl=${props.imageUrl}&description=${props.prompt}`}
-                  className="w-full"
-                >
-                  <Button className="w-full gap-1" variant="outline">
-                    <Plus className="h-4 w-4" />
-                    {t("Character")}
-                  </Button>
-                </Link>
-              </Tooltip>
               {props.imageUrl && (
                 <a
                   href={props.imageUrl}
@@ -189,22 +189,20 @@ const ImageCard = (props: {
               )}
             </div>
 
-            {!props?.isNSFW && (
-              <Tooltip content={"Create new image by remixing this image"}>
-                <Link
-                  href={`/images?prompt=${props.prompt}&model=${props.model}`}
-                  className="absolute right-4 top-4 z-[4] hidden items-center group-hover:flex"
+            <Tooltip content={"Create new image by remixing this image"}>
+              <Link
+                href={`/images?prompt=${props.prompt}&model=${props.model}`}
+                className="absolute right-4 top-4 z-[4] hidden items-center group-hover:flex"
+              >
+                <Button
+                  variant="outline"
+                  className="h-5 rounded-full border-none text-xs md:text-[10px]"
                 >
-                  <Button
-                    variant="outline"
-                    className="h-5 rounded-full border-none text-xs md:text-[10px]"
-                  >
-                    <Repeat className="h-3 w-3 p-0.5" />
-                    {t("Remix")}
-                  </Button>
-                </Link>
-              </Tooltip>
-            )}
+                  <Repeat className="h-3 w-3 p-0.5" />
+                  {t("Remix")}
+                </Button>
+              </Link>
+            </Tooltip>
             {props.imageUrl && (
               <>
                 {props?.isNSFW ? (
