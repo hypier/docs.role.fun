d7628ba5cadf01bab11b2c6c635085f293e74cba
barry <hypier@qq.com>
2024-06-12 18:09:37 +0800

    images

--- apps/web/convex/r2.ts
+++ apps/web/convex/r2.ts
@@ -23,8 +23,6 @@ export const getUploadUrl = async (filename: string) => {
   };
 
   const command = new PutObjectCommand(params);
-
-  console.log("getUploadUrl params", params, process.env.R2_BUCKET_NAME, process.env.R2_ENDPOINT);
   const signedUrl = await getSignedUrl(s3Client, command, {
     expiresIn: 60,
   });
