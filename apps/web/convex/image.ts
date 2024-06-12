ce415a7e383d2f471ffab9423a361b40b177d721
barry <hypier@qq.com>
2024-06-12 12:11:31 +0800

    images

--- apps/web/convex/image.ts
+++ apps/web/convex/image.ts
@@ -396,7 +396,11 @@ export const generateByPrompt = internalAction(
       console.log("Image uploaded, extracting image URL");
       const urlParts = response.url.split("/");
       const filename = urlParts[urlParts.length - 1];
-      const imageUrl = `https://r2.openroleplay.ai/${filename}`;
+      const domain =process.env.NODE_ENV === "development"
+          ? process.env.R2_DOMAIN_DEV
+          : process.env.R2_DOMAIN;
+      const imageUrl = `${domain}/${filename}`;
+      // const imageUrl = `https://r2.openroleplay.ai/${filename}`;
       console.log(`Image URL extracted: ${imageUrl}`);
       await ctx.runMutation(internal.images.uploadR2Image, {
         imageId,
@@ -435,7 +439,10 @@ export const upload = action({
     console.log("Image uploaded, extracting image URL");
     const urlParts = response.url.split("/");
     const uploadedFilename = urlParts[urlParts.length - 1];
-    const imageUrl = `https://r2.openroleplay.ai/${uploadedFilename}`;
+    const domain =process.env.NODE_ENV === "development"
+        ? process.env.R2_DOMAIN_DEV
+        : process.env.R2_DOMAIN;
+    const imageUrl = `${domain}/${uploadedFilename}`;
     console.log(`Image URL extracted: ${imageUrl}`);
     return imageUrl;
   },
