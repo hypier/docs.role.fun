284de352e6785162533ed52cbb9fa82ed3ecc784
barry <hypier@qq.com>
2024-06-26 20:30:42 +0800

    去掉console.log

--- apps/web/app/crystals/payment-dialog.tsx
+++ apps/web/app/crystals/payment-dialog.tsx
@@ -19,7 +19,7 @@ import { Drawer, DrawerContent } from "@repo/ui/src/components/drawer";
 
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string;
 
-console.log("stripeKey: ", stripeKey)
+// console.log("stripeKey: ", stripeKey)
 
 const stripePromise = loadStripe(stripeKey);
 
