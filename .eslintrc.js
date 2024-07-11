04223f8a8dbfc196eccfef34edaa68bcaa790ff1
hypier <hypier@qq.com>
2024-07-11 11:36:20 +0800

    更新ESLint配置和sitemap.ts，提升开发体验和内容准确性
    - 在.web和UI项目的.eslintrc.js中整合了"turbo"配置，以提升开发效率。
    - 修改sitemap.ts中角色的changeFrequency为"daily"，确保内容更新频率的准确性。
    - 解决了UI包中ESLint规则与项目需求的冲突，关闭了不必要的规则。

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["@repo/eslint-config/library.js","eslint-config-turbo"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 };
