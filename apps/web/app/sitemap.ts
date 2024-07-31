e2d9349c5540ab8ad8930963261f60a989fa0334
hypier <hypier@qq.com>
2024-07-31 15:34:48 +0800

    sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -8,42 +8,42 @@ const client = new ConvexHttpClient(
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const characters = await client.query(api.characters.listBackend, {});
-  // const models = await client.query(api.characters.listAllModels);
+  const models = await client.query(api.characters.listAllModels);
   const images = await client.query(api.public.listImages, {});
 
   return [
     {
-      url: "https://Role.fun",
+      url: "https://role.fun",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://Role.fun/models",
+      url: "https://role.fun/models",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://Role.fun/crystals",
+      url: "https://role.fun/crystals",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     ...characters.map((character) => ({
-      url: `https://Role.fun/character/${character._id}`,
+      url: `https://role.fun/character/${character._id}`,
       lastModified: new Date(character.updatedAt),
       changeFrequency: "daily" as "daily",
       priority: 0.8,
     })),
-    // ...models.map((character) => ({
-    //   url: `https://Role.fun/character/${character._id}`,
-    //   lastModified: new Date(character.updatedAt),
-    //   changeFrequency: "daily" as "daily",
-    //   priority: 0.8,
-    // })),
+    ...models.map((character) => ({
+      url: `https://role.fun/character/${character._id}`,
+      lastModified: new Date(character.updatedAt),
+      changeFrequency: "daily" as "daily",
+      priority: 0.8,
+    })),
     ...images.map((image) => ({
-      url: `https://Role.fun/image/${image._id}`,
+      url: `https://role.fun/image/${image._id}`,
       lastModified: new Date(image._creationTime),
       changeFrequency: "daily" as "daily",
       priority: 0.8,
